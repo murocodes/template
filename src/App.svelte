60557ef783d798442b1db33c3a3f606ec5c312e4
Murat Tasdemir <tasdemirmurat90@gmail.com>
2022-03-12 23:13:03 +0100

    test

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hallo {name}!</h1>
+	<p>Das ist ein nicht funktionaler Link <a href="https://führt.zu.nichts">Link</a></p>
 </main>
 
 <style>
